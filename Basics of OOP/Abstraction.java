1b7d03d7826d17b695df3414a85c88a672adfba0
Hanan Qaisar <hananqaisar316@gmail.com>
2026-03-02 19:13:34 +0500

    Create object array

--- Basics of OOP/Abstraction.java
+++ Basics of OOP/Abstraction.java
@@ -1,33 +1,100 @@
 
-class Employee {
-    public void claculateSalary() {
-        System.out.print("\nTeacher salary is calculating ");
-        for (int i = 0; i < 3; i++) {
+import java.util.Scanner;
 
-            System.out.print(".");
-        }
+abstract class Employee {
+
+    private String subject;
+
+    abstract public void calculateSalary();
+
+    void setSub(String subject) {
+        this.subject = subject;
     }
-    // System.out.print(("\b\b\b"));
-    // System.out.print((" "));
-    // System.out.print(("\b\b\b"));
+
+    String getSub() {
+        return subject;
+    }
 }
 
-class MathTeacher extends Employee {
+class Teacher extends Employee {
+
     @Override
-    public void claculateSalary() {
-        System.out.println("\nMath teacher salary is calculating");
+    public void calculateSalary() {
+        System.out.println("\nTeacher salary is calculating...\n");
+
     }
+}
+
+class Engineer extends Employee {
+
+    public Engineer(String subject) {
+
+        setSub(subject);
+    }
+
+    @Override
+    public void calculateSalary() {
+        System.out.println("Engineer salary is calculating...\n");
+    }
+
+    public void subject() {
+        System.out.println("Subject: " + getSub());
+    }
+
+}
+
+class Doctor {
+
+    String subject;
+
+    public Doctor(String subject) {
+        this.subject = subject;
+    }
+
+    public void calculateSalary() {
+        System.out.println("Doctor Salary is calculating...");
+    }
+
 }
 
 public class Abstraction {
     public static void main(String[] args) {
 
-        MathTeacher e1 = new MathTeacher();
+        Scanner input = new Scanner(System.in);
 
-        e1.claculateSalary();// this is called overRiding or run time polymorphisnm
+        // For teacher
+        System.out.print("Enter subject: ");
+        Teacher t1 = new Teacher();
+        String hold = input.nextLine();
+        t1.setSub(hold);
+        System.out.print("Subject:" + t1.getSub());
+        t1.calculateSalary();
 
-        Employee t1 = new Employee();
-        t1.claculateSalary();
+        // For Engineer
+        Engineer e1 = new Engineer("Math");
+        e1.subject();
+        e1.calculateSalary();
 
+        // for doctor
+        System.out.print("Enter strenght of doctor? ");
+        int n = input.nextInt();
+        input.nextLine();// it clear previous \n in the buffer
+
+        Doctor[] doctor = new Doctor[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("enter subject " + (i + 1) + " :");
+
+            String su = input.nextLine();
+            doctor[i] = new Doctor(su);
+
+        }
+        System.out.println("");// newline
+        for (int i = 0; i < n; i++) {
+
+            System.out.println("Doctor " + (i + 1) + " Subject: " + doctor[i].subject);
+
+        }
+
+        input.close();
     }
 }
