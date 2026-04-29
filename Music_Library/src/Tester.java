207dc278dcb7d8a644aa69283ee595a4e599d91e
Hanan Qaisar (Linux) <hananqaisar316@gmail.com>
2026-04-29 16:53:49 +0500

    Test all functionalities in main

--- Music_Library/src/Tester.java
+++ Music_Library/src/Tester.java
@@ -6,32 +6,38 @@ public class Tester {
         Scanner input = new Scanner(System.in);
 
         // Create Media objects
-        Media m1 = new Song("Dill lagi", 134, 2019, "Sad");
-        Media m2 = new Song("Dil Lagi", 345, 2020, "Romantic");
-        Media m3 = new PodcastEp("Podcast no.1", 3235, 2026, "Podcast", 3);
-        Media m4 = new Song("End of beginning", 230, 2018, "Emotional");
+        Media m1 = new Song("Dil Lagi", 134, 2019, "Sad");
+        Media m2 = new Song("Dil Lagi (Romantic Version)", 345, 2020, "Romantic");
+        Media m3 = new PodcastEp("Podcast No. 1: Introduction to Python", 3235, 2021, "Podcast", 3);
+        Media m4 = new Song("The End of Beginning", 230, 2018, "Emotional");
 
         // artists
-        m1.artist = new Artist("Atif Aslam", "Pakistan", 2300425);
-        m2.artist = new Artist("Arigit Singh", "India", 73004325);
-        m3.artist = new Artist("Atif aslam", "Pakistan", 13423);
+        m1.artist = new Artist("Atif Aslam", "Pakistan", 230320425);
+        m2.artist = new Artist("Arijit Singh", "India", 73004325);
+        m3.artist = new Artist("Atif Aslam", "Pakistan", 1342343);
+        m4.artist = new Artist("Steave", "Unknown", 953342334);
 
         // Playlist
-        Playlist likedSongs = new Playlist("Liked songs");
+        Playlist likedSongs = new Playlist("Favorite Songs");
         likedSongs.addItems(m2);
         likedSongs.addItems(m1);
-        likedSongs.addItems(m3);
         likedSongs.addItems(m4);
 
-        Playlist personal = new Playlist("Personal choice");
+        Playlist personal = new Playlist("Sad Songs");
         personal.addItems(m2);
         personal.addItems(m3);
 
-        System.out.print("Compare m1 with m3: ");
+        System.out.println("===============Playlist=============");
+        System.out.println(likedSongs);
+        System.out.println(personal);
+
+        System.out.println("=============Comparision============");
+        System.out.printf("Compare %s with %s: ", m1.getArtist(), m3.getArtist());
         System.out.println(m1.artist.equals(m3.artist)); // true
-        System.out.print("Compare m1 with m2: ");
+        System.out.printf("Comapre %s with %s: ", m1.getArtist(), m2.getArtist());
         System.out.println(m1.artist.equals(m2.artist)); // false
 
+        // creating list to store all media's ans then sort it
         List<Media> list = new ArrayList<>();
         list.add(m4);
         list.add(m1);
@@ -42,11 +48,10 @@ public class Tester {
         Collections.sort(list);
 
         // Print sorted list through Collections
+        System.out.println("<<<<==============---Sorting by Title & release year---=============>>>> ");
         System.out.println("Sorted list:");
         for (Media media : list) {
-            System.out.printf("%-30s - %-4d\n",
-                    media.getTitle(),
-                    media.getReleaseYear());
+            System.out.println(media);
         }
 
         // Play all playlist
@@ -56,6 +61,20 @@ public class Tester {
         // size of playlist
         likedSongs.getSize(likedSongs);
         personal.getSize(personal);
+
+        System.out.println("<<<<==============---Sorting by Durations(Accending)---=============>>>> ");
+        Collections.sort(list, new sortByduration());
+        System.out.println("Sorted list:");
+        for (Media media : list) {
+            System.out.println(media);
+        }
+
+        System.out.println("<<<<==============---Sorting by Release Years(Decending)---=============>>>> ");
+        Collections.sort(list, new sortByreleaseYear());
+        System.out.println("Sorted list:");
+        for (Media media : list) {
+            System.out.println(media);
+        }
 
         input.close();
     }
