b1f11a360eafdda836599e53944b0acb5046479f
Hanan Qaisar (Linux) <hananqaisar316@gmail.com>
2026-05-06 23:27:51 +0500

    Create Comparator interface with 3 uses

---
+++ BasicOOP/Polymorphism/Product_Sorting/Comparator.java
@@ -0,0 +1,33 @@
+package Product_Sorting;
+
+import java.util.Comparator;
+
+class sortByPrice implements Comparator<Product> {
+
+    @Override
+    public int compare(Product t, Product other) {
+        int result = Double.compare(t.getPrice(), other.getPrice());
+        return result;
+    }
+
+}
+
+class sortByRating implements Comparator<Product> {
+
+    @Override
+    public int compare(Product t, Product other) {
+        int result = Double.compare(other.getCount_rating(), t.getCount_rating());
+        return result;
+    }
+
+}
+
+class sortByAlphabets implements Comparator<Product> {
+
+    @Override
+    public int compare(Product t, Product other) {
+        int result = t.getName().compareToIgnoreCase(other.getName());
+        return result;
+    }
+
+}
